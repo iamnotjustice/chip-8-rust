11f86b47de01ddbe70caa4c72dad4803f33cf961
iamnotjustice <nevermindjustice@gmail.com>
2023-08-30 12:53:54 +0300

    Drawing on a proper window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use piston_window::*;
+
 use std::io::prelude::*;
 use std::fs::File;
 use rand::Rng;
@@ -24,6 +26,8 @@ struct CPU {
     // the display is 64 by 32
     display: [[u8; 64]; 32],
 
+    window: PistonWindow,
+
     index_register: usize,
 }
 
@@ -39,85 +43,83 @@ impl CPU {
         (op_byte1 << 8) | op_byte2
     }
 
-    fn run(&mut self, original: bool) {
-        loop {
-            let opcode = self.read_opcode();
-            self.position_in_memory += 2;
+    fn iter(&mut self, original: bool) {
+        let opcode = self.read_opcode();
+        self.position_in_memory += 2;
 
-            //let c = ((opcode & 0xf000) >> 12) as u8;
-            let x = ((opcode & 0x0f00) >> 8) as u8;
-            let y = ((opcode & 0x00f0) >> 4) as u8;
-            let d = ((opcode & 0x000f) >> 0) as u8;
+        //let c = ((opcode & 0xf000) >> 12) as u8;
+        let x = ((opcode & 0x0f00) >> 8) as u8;
+        let y = ((opcode & 0x00f0) >> 4) as u8;
+        let d = ((opcode & 0x000f) >> 0) as u8;
 
-            let op_minor = (opcode & 0x000f) as u8;
+        let op_minor = (opcode & 0x000f) as u8;
 
-            // take 3 rightmost nibbles
-            let nnn = opcode & 0x0fff;
-            // and 2 rightmost nibbles
-            let kk = opcode & 0x00ff;
+        // take 3 rightmost nibbles
+        let nnn = opcode & 0x0fff;
+        // and 2 rightmost nibbles
+        let kk = opcode & 0x00ff;
 
-            match opcode {
-                // finish exec opcode
-                0x0000 => {
-                    return;
-                }
-                // return opcode
-                0x00ee => self.ret(),
-                // clear the display
-                0x00e0 => self.clr(),
-                // jump opcodes
-                0x1000..=0x1fff => self.jmp(nnn),
-                // call opcode
-                0x2000..=0x2fff => self.call(nnn),
-                0x3000..=0x3fff => self.skip_eq(x, kk),
-                0x4000..=0x4fff => self.skip_neq(x, kk),
-                0x5000..=0x5fff => self.skip_eq_xy(x, y),
-                0x6000..=0x6fff => self.set(x, kk),
-                0x7000..=0x7fff => self.add(x, kk),
-                // two operand opcodes
-                0x8000..=0x8fff => {
-                    match op_minor {
-                        0 => self.set_xy(x, y),
-                        1 => self.bin_or_xy(x, y),
-                        2 => self.bin_and_xy(x, y),
-                        3 => self.bin_xor_xy(x, y),
-                        4 => self.add_xy(x, y),
-                        5 => self.sub_xy(x, y, true),
-                        6 => self.shift_xy(x, y, true, original),
-                        7 => self.sub_xy(x, y, false),
-                        0xe => self.shift_xy(x, y, false, original),
-                        _ => todo!("opcode {:04x}", opcode),
-                    }
-                }
-                0x9000..=0x9fff => self.skip_neq_xy(x, y),
-                // set index register opcodes
-                0xa000..=0xafff => self.set_idx(nnn),
-                0xb000..=0xbfff => self.jmp_w_offset(x, nnn, original),
-                0xc000..=0xcfff => self.rand(x, kk),
-                0xd000..=0xdfff => self.to_display(x, y, d),
-                0xe000..=0xefff => {
-                    match kk {
-                        0x00a1 => self.skip_not_pressed(x),
-                        0x009e => self.skip_pressed(x),
-                        _ => todo!("opcode {:04x}", opcode),
-                    }
-                }
-                0xf000..=0xffff => {
-                    match kk {
-                        0x0007 => self.get_delay(x),
-                        0x0015 => self.set_delay(x),
-                        0x0018 => self.set_sound(x),
-                        0x001e => self.add_to_idx(x),
-                        0x000a => self.get_key(x),
-                        0x0029 => self.get_font_char(x),
-                        0x0033 => self.bin_dec_conversion(x),
-                        0x0055 => self.store_reg_to_mem(x, original),
-                        0x0065 => self.get_from_mem_to_reg(x, original),
-                        _ => todo!("opcode {:04x}", opcode),
-                    }
-                }
-                _ => todo!("opcode {:04x}", opcode),
+        match opcode {
+            // finish exec opcode
+            0x0000 => {
+                return;
             }
+            // return opcode
+            0x00ee => self.ret(),
+            // clear the display
+            0x00e0 => self.clr(),
+            // jump opcodes
+            0x1000..=0x1fff => self.jmp(nnn),
+            // call opcode
+            0x2000..=0x2fff => self.call(nnn),
+            0x3000..=0x3fff => self.skip_eq(x, kk),
+            0x4000..=0x4fff => self.skip_neq(x, kk),
+            0x5000..=0x5fff => self.skip_eq_xy(x, y),
+            0x6000..=0x6fff => self.set(x, kk),
+            0x7000..=0x7fff => self.add(x, kk),
+            // two operand opcodes
+            0x8000..=0x8fff => {
+                match op_minor {
+                    0 => self.set_xy(x, y),
+                    1 => self.bin_or_xy(x, y),
+                    2 => self.bin_and_xy(x, y),
+                    3 => self.bin_xor_xy(x, y),
+                    4 => self.add_xy(x, y),
+                    5 => self.sub_xy(x, y, true),
+                    6 => self.shift_xy(x, y, true, original),
+                    7 => self.sub_xy(x, y, false),
+                    0xe => self.shift_xy(x, y, false, original),
+                    _ => todo!("opcode {:04x}", opcode),
+                }
+            }
+            0x9000..=0x9fff => self.skip_neq_xy(x, y),
+            // set index register opcodes
+            0xa000..=0xafff => self.set_idx(nnn),
+            0xb000..=0xbfff => self.jmp_w_offset(x, nnn, original),
+            0xc000..=0xcfff => self.rand(x, kk),
+            0xd000..=0xdfff => self.to_display(x, y, d),
+            0xe000..=0xefff => {
+                match kk {
+                    0x00a1 => self.skip_not_pressed(x),
+                    0x009e => self.skip_pressed(x),
+                    _ => todo!("opcode {:04x}", opcode),
+                }
+            }
+            0xf000..=0xffff => {
+                match kk {
+                    0x0007 => self.get_delay(x),
+                    0x0015 => self.set_delay(x),
+                    0x0018 => self.set_sound(x),
+                    0x001e => self.add_to_idx(x),
+                    0x000a => self.get_key(x),
+                    0x0029 => self.get_font_char(x),
+                    0x0033 => self.bin_dec_conversion(x),
+                    0x0055 => self.store_reg_to_mem(x, original),
+                    0x0065 => self.get_from_mem_to_reg(x, original),
+                    _ => todo!("opcode {:04x}", opcode),
+                }
+            }
+            _ => todo!("opcode {:04x}", opcode),
         }
     }
 
@@ -151,8 +153,9 @@ impl CPU {
         }
     }
 
-    fn get_key(&mut self, x: u8) {
-        // if key is pressed -> put key val in Vx
+    // TODO
+    fn get_key(&mut self, _x: u8) {
+        // if key is pressed -> put key val in v_x
         // if not - decrement PC, cause this operation blocks
         // until a key is pressed
 
@@ -166,7 +169,7 @@ impl CPU {
 
     fn get_font_char(&mut self, x: u8) {
         // put an addr of hexademical char
-        // from "vx" to idx register
+        // from "v_x" to idx register
         let char = self.registers[x as usize];
 
         // our fonts start at FONT_OFFSET and we need to offset from there
@@ -211,14 +214,16 @@ impl CPU {
         self.sound_timer = arg;
     }
 
-    fn skip_pressed(&mut self, x: u8) {
-        // check vX register and see if the key equal to vX value is pressed
+    // TODO
+    fn skip_pressed(&mut self, _x: u8) {
+        // check v_x register and see if the key equal to v_x value is pressed
         // if it is -> skip (PC += 2)
         // valid keypad keys are [0 - F]
     }
 
-    fn skip_not_pressed(&mut self, x: u8) {
-        // check vX register and see if the key equal to vX value is pressed
+    // TODO
+    fn skip_not_pressed(&mut self, _x: u8) {
+        // check v_x register and see if the key equal to v_x value is pressed
         // if it is not -> skip (PC += 2)
     }
 
@@ -243,18 +248,18 @@ impl CPU {
         let arg2 = self.registers[y as usize];
 
         let val: u8;
-        let mut flipVf: bool = false;
+        let mut flip_vf: bool = false;
         let overflow: bool;
 
         if order {
             (val, overflow) = arg1.overflowing_sub(arg2);
             if arg1 > arg2 && !overflow {
-                flipVf = true;
+                flip_vf = true;
             }
         } else {
             (val, overflow) = arg2.overflowing_sub(arg1);
             if arg2 > arg1 && !overflow {
-                flipVf = true;
+                flip_vf = true;
             }
         }
 
@@ -262,7 +267,7 @@ impl CPU {
 
         self.registers[0xf] = 0;
 
-        if flipVf {
+        if flip_vf {
             self.registers[0xf] = 1;
         }
     }
@@ -272,29 +277,29 @@ impl CPU {
             self.registers[x as usize] = self.registers[y as usize];
         }
 
-        let Vf: u8;
+        let v_f: u8;
 
         if order {
-            Vf = self.registers[x as usize] & 0b0000_0001;
+            v_f = self.registers[x as usize] & 0b0000_0001;
             self.registers[x as usize] >>= 1;
         } else {
             // get the bit which is going to be shifted out
-            Vf = self.registers[x as usize] & 0b1000_0000;
+            v_f = self.registers[x as usize] & 0b1000_0000;
             self.registers[x as usize] <<= 1;
         }
 
-        self.registers[0xf] = Vf;
+        self.registers[0xf] = v_f;
     }
 
     fn jmp_w_offset(&mut self, x: u8, nnn: u16, original: bool) {
         if original {
-            let V0 = self.registers[0x0];
+            let v_0 = self.registers[0x0];
 
-            self.position_in_memory = (nnn as usize) + (V0 as usize);
+            self.position_in_memory = (nnn as usize) + (v_0 as usize);
         } else {
-            let Vx = self.registers[x as usize];
+            let v_x = self.registers[x as usize];
 
-            self.position_in_memory = (nnn as usize) + (Vx as usize);
+            self.position_in_memory = (nnn as usize) + (v_x as usize);
         }
     }
 
@@ -432,25 +437,21 @@ impl CPU {
         self.position_in_memory = call_addr as usize;
     }
 
-    fn redraw(&self) {
-        for row in self.display {
-            for pixel in row {
-                if pixel == 1 {
-                    print!("X");
-                } else {
-                    print!(" ");
-                }
-            }
-            println!("");
-        }
-        println!("frame end");
-    }
+    fn redraw(&mut self) {}
 }
 
+// TODO: two OPCODES are broken (F55 & F33) and input-based OPCODES are not yet implemented.
 fn main() {
+    let (width, height) = (64, 32);
+
+    let window: PistonWindow = WindowSettings::new("chip8", [width * 4, height * 4])
+        .exit_on_esc(true)
+        .build()
+        .expect("could not create window");
+
     let args: Vec<String> = env::args().collect();
 
-    let file_path = &args[2];
+    let file_path = &args[1];
 
     let mut f = File::open(file_path).unwrap();
 
@@ -467,6 +468,7 @@ fn main() {
         stack: [0; 16],
         stack_pointer: 1,
         display: [[0; 64]; 32],
+        window: window,
         index_register: 0x000,
     };
 
@@ -564,8 +566,41 @@ fn main() {
         cpu.memory[i] = buffer[i - 0x200];
     }
 
-    // a switch between original VIP int. behavior and newer ones
-    cpu.run(false);
+    // TODO: decouple CPU opcode runs from window refreshes
+    // and add separate refresh control for both.
+    while let Some(_) = handle_window(&mut cpu) {
+        cpu.iter(true);
+    }
+}
 
-    println!("{}", cpu.registers[0]);
+fn handle_window(cpu: &mut CPU) -> Option<Event> {
+    let e = &cpu.window.next();
+    match e {
+        Some(event) => {
+            cpu.window.draw_2d(event, |ctx, renderer, _device| {
+                clear([0.15, 0.17, 0.17, 0.9], renderer);
+
+                for x in (0..64 * 4).step_by(4) {
+                    for y in (0..32 * 4).step_by(4) {
+                        let size = [f64::from((x as i32) + 4), f64::from((y as i32) + 4), 4.0, 4.0];
+
+                        rectangle(
+                            [
+                                cpu.display[y / 4][x / 4] as f32,
+                                cpu.display[y / 4][x / 4] as f32,
+                                cpu.display[y / 4][x / 4] as f32,
+                                cpu.display[y / 4][x / 4] as f32,
+                            ],
+                            size,
+                            ctx.transform,
+                            renderer
+                        );
+                    }
+                }
+            });
+        }
+        None => {}
+    }
+
+    e.clone()
 }
